f7cac2656c82aa1e465eceacc295db527bb06f13
prasanth <34934697+prasanthcodes@users.noreply.github.com>
2025-04-12 12:06:05 +0530

    Update scene_editor.py

--- scene_editor.py
+++ scene_editor.py
@@ -992,42 +992,7 @@ class LookingDemo(ShowBase):
         self.dlight1.node().get_lens().setNearFar(1, 50)
         self.dlight1.node().show_frustum()
         self.render.setLight(self.dlight1)
-        
-
-    def filter_lens_flare(self):
-        self.setBackgroundColor(0.1,0.1,0.1)
-        # ATI video cards (or drivers) are not too frendly with the input 
-        # variables, so I had to transfer most of parameters to the shader
-        # code.
-
-        # Threshold (x,y,z) and brightness (w) settings
-        threshold = Vec4(0.4, 0.4, 0.4, 0.3) # <----
-        
-        # FilterManager
-        manager = self.FilterManager_1
-        tex1 = Texture()
-        tex2 = Texture()
-        tex3 = Texture()
-        finalquad = manager.renderSceneInto(colortex=tex1)
-        # First step - threshold and radial blur
-        interquad = manager.renderQuadInto(colortex=tex2)
-        interquad.setShader(Shader.load("invert_threshold_r_blur.sha"))
-        interquad.setShaderInput("tex1", tex1)
-        interquad.setShaderInput("threshold", threshold)
-        # Second step - hardcoded fast gaussian blur. 
-        # Not very important. This step can be omitted to improve performance
-        # with some minor changes in lens_flare.sha
-        interquad2 = manager.renderQuadInto(colortex=tex3)
-        interquad2.setShader(Shader.load("gaussian_blur.sha"))
-        interquad2.setShaderInput("tex2", tex2)
-        # Final - Make lens flare and blend it with the main scene picture
-        finalquad.setShader(Shader.load("lens_flare.sha"))
-        finalquad.setShaderInput("tex1", tex1)
-        finalquad.setShaderInput("tex2", tex2)
-        finalquad.setShaderInput("tex3", tex3)
-        #lf_settings = Vec3(lf_samples, lf_halo_width, lf_flare_dispersal)
-        #finalquad.setShaderInput("lf_settings", lf_settings)
-        #finalquad.setShaderInput("lf_chroma_distort", lf_chroma_distort)                                              
+                                
     def camera_rotate(self,task):
         # Check to make sure the mouse is readable
         if self.mouseWatcherNode.hasMouse():
@@ -1047,10 +1012,7 @@ class LookingDemo(ShowBase):
 
                 # Update camera angles based on mouse movement
                 self.cameraAngleH -= dx * self.mouse_sensitivity * globalClock.getDt()
-                self.cameraAngleP -= dy * self.mouse_sensitivity * globalClock.getDt()
-
-                # Clamp pitch to avoid flipping
-                self.cameraAngleP = max(-90, min(90, self.cameraAngleP))
+                self.cameraAngleP -= dylf.cameraAngleP = max(-90, min(90, self.cameraAngleP))
                 
                 #self.camera.setPos(camX, camY, camZ)
                 self.camera.setHpr(self.cameraAngleH, self.cameraAngleP, 0)
